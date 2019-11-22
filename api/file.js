17675d36ec973fc94a26cfdd2440e4e34ec46c15
liyi <liyi@sinux.com>
2019-11-22 15:04:26 +0800

    fileparse

---
+++ api/file.js
@@ -0,0 +1,28 @@
+const xlsx = require('xlsx');
+let parse = require('co-busboy');
+
+modules.exports = function batchInsertFile(formData) {
+    let file = parse(formData);
+    const workbook = getFile(file);
+    const sheetsName = workbook.SheetNames;
+    const datas = [];
+    for(const sheetName of sheetsName) {
+        const worksheet = workbook.Sheet[sheetName];
+        const data = xlsx.utils.sheet_add_json(worksheet);
+        datas.push(data);
+    }
+}
+
+function getFile(stream) {
+    return new Promise((resolve, reject) => {
+        let buffers = [];
+        stream.on('data', function(data) {
+            buffers.push(data);
+        })
+        stream.on('end', function() {
+            let buffer = Buffer.concat(buffers);
+            let workbook = xlsx.read(buffer, {type: 'buffer'});
+            resolve(workbook)
+        })
+    })
+}
